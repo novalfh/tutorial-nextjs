556b309b7ecaecab183a65411789fd37ffb2e619
novalfh <annaufal1521@gmail.com>
2024-03-05 18:43:57 +0700

    my

---
+++ components/navbar/Navbar.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const Navbar = () => {
+  return (
+    <nav>
+        <h1>Hello There</h1>
+        <h1>Hello There</h1>
+        <h1>Hello There</h1>
+
+    </nav>
+  )
+}
+
+export default Navbar
